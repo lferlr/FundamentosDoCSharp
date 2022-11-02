ce477ee54b7e73808e60dc962dee9586e71a333b
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-11-02 19:29:59 -0300

    Inciando o mão na massa do curso de Dapper

---
+++ Blog/Program.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+  class Program
+  {
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r$#@";
+    public static void ReadUsers()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var users = connection.GetAll<User>();
+
+        foreach (var user in users)
+        {
+          Console.WriteLine(user.Name);
+        }
+      }
+    }
+  }
+}

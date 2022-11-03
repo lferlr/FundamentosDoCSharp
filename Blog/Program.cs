4f04fe1965a0f9d974d43083651848e7a9d6665c
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-11-02 21:31:37 -0300

    Realizando o CRUD mais básico

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -6,7 +6,17 @@ namespace Blog
 {
   class Program
   {
-    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r$#@";
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r$#@;TrustServerCertificate=True";
+
+    static void Main(string[] args)
+    {
+      // ReadUsers();
+      // ReadUser();
+      // CreateUser();
+      // UpdateUser();
+      DeleteUser();
+    }
+
     public static void ReadUsers()
     {
       using (var connection = new SqlConnection(CONNECTION_STRING))
@@ -17,6 +27,63 @@ namespace Blog
         {
           Console.WriteLine(user.Name);
         }
+      }
+    }
+    public static void ReadUser()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(1);
+
+        Console.WriteLine(user.Name);
+      }
+    }
+    
+    public static void CreateUser()
+    {
+      var user = new User()
+      {
+        Bio = "Equipe balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+      
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Insert<User>(user);
+        Console.WriteLine("Cadastro realizado com sucesso!");
+      }
+    }
+    
+    public static void UpdateUser()
+    {
+      var user = new User()
+      {
+        Bio = "Equipe balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+      
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Update<User>(user);
+        Console.WriteLine("Atualização realizada com sucesso!");
+      }
+    }
+    
+    public static void DeleteUser()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(2);
+        connection.Delete<User>(user);
+        Console.WriteLine("Exclusão realizada com sucesso!");
       }
     }
   }
